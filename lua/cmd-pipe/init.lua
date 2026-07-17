fda755fb0158b6583e4dcc664145f57fa9d4156e
Yuri Andriaccio <yurand2000@gmail.com>
2026-07-17 17:47:53 +0200

    Run Pipe command from Buffer

--- lua/cmd-pipe/init.lua
+++ lua/cmd-pipe/init.lua
@@ -13,23 +13,35 @@ function M.setup(module_opts)
 		return lines
 	end
 
-	local pipe_fn = function(args, opts)
+	local read_buffer = function(buffer_id)
+		local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, -1, false)
+		return buf_data_to_string(lines)
+	end
+
+	local split_at_whitespaces = function(str)
+		local t = {}
+		for w in string.gmatch(str, "%S+") do
+			table.insert(t, w)
+		end
+		return t
+	end
+
+	local pipe_fn = function(name, args, opts)
 		-- Get current buffer contents
 		local curr_buf = vim.api.nvim_get_current_buf()
-		local curr_lines = vim.api.nvim_buf_get_lines(curr_buf, 0, -1, false)
-		local curr_text = buf_data_to_string(curr_lines)
+		local curr_text = read_buffer(curr_buf)
 
 		-- Get explicit file buffer contents
 		local buffer_file_pattern = "^%$%$(%d+)$"
 		local self_buffer_file_pattern = "^%$%$$"
 		local tmpdir = vim.uv.os_tmpdir()
-		for argno = 1, #args.fargs, 1 do
+		for argno = 1, #args, 1 do
 			local buf_id = nil
-			if string.match(args.fargs[argno], self_buffer_file_pattern) then
+			if string.match(args[argno], self_buffer_file_pattern) then
 				buf_id = vim.api.nvim_get_current_buf()
 			end
 			if not buf_id then
-				buf_id = tonumber(string.match(args.fargs[argno], buffer_file_pattern), 10)
+				buf_id = tonumber(string.match(args[argno], buffer_file_pattern), 10)
 			end
 
 			if not buf_id then
@@ -38,21 +50,20 @@ function M.setup(module_opts)
 
 			if not vim.api.nvim_buf_is_valid(buf_id) then
 				vim.notify(
-					string.format("'%s %s' unknown buffer %s", args.name, args.fargs[1], args.fargs[argno]),
+					string.format("'%s %s' unknown buffer %s", name, args[1], args[argno]),
 					vim.log.levels.ERROR
 				)
-				print(string.format("Unknown buffer: %s", args.fargs[argno]))
+				print(string.format("Unknown buffer: %s", args[argno]))
 				return
 			end
 
-			local lines = vim.api.nvim_buf_get_lines(buf_id, 0, -1, false)
-			local text = buf_data_to_string(lines)
+			local text = read_buffer(buf_id)
 
 			-- Create temporary file
 			local fd, path = vim.uv.fs_mkstemp(tmpdir .. "/XXXXXX")
 			if not fd then
 				vim.notify(
-					string.format("'%s %s' internal error: failed creating tmp file", args.name, args.fargs[1]),
+					string.format("'%s %s' internal error: failed creating tmp file", name, args[1]),
 					vim.log.levels.ERROR
 				)
 				print(string.format("Internal error: failed creating tmp file %s; err: %s", tmpdir .. "XXXXXX", path))
@@ -62,20 +73,20 @@ function M.setup(module_opts)
 			local _, err = vim.uv.fs_write(fd, text)
 			if err then
 				vim.notify(
-					string.format("'%s %s' internal error: failed writing to tmp file", args.name, args.fargs[1]),
+					string.format("'%s %s' internal error: failed writing to tmp file", name, args[1]),
 					vim.log.levels.ERROR
 				)
 				print(string.format("Internal error: failed writing to tmp file %s; err: %s", path, err))
 				return
 			end
 
-			args.fargs[argno] = path
+			args[argno] = path
 
 			::continue::
 		end
 
 		-- Run shell command
-		local sh_args = table.concat(args.fargs, " ")
+		local sh_args = table.concat(args, " ")
 		local res = { stdout = "", stderr = "" }
 
 		-- Create timer for Async notifications
@@ -84,7 +95,7 @@ function M.setup(module_opts)
 		local print_delay_ms = 1000
 		if not timer then
 			vim.notify(
-				string.format("'%s %s' internal error: failed creating timer", args.name, args.fargs[1]),
+				string.format("'%s %s' internal error: failed creating timer", name, args[1]),
 				vim.log.levels.ERROR
 			)
 			print("Internal error: failed creating timer")
@@ -99,7 +110,7 @@ function M.setup(module_opts)
 		local on_exit = function()
 			if res.code ~= 0 then
 				vim.notify(
-					string.format("'%s %s' execution error...", args.name, args.fargs[1]),
+					string.format("'%s %s' execution error...", name, args[1]),
 					vim.log.levels.ERROR
 				)
 				print(string.format("Error executing: %s", sh_args))
@@ -111,7 +122,7 @@ function M.setup(module_opts)
 			local new_buf = vim.api.nvim_create_buf(true, false)
 			if new_buf == 0 then
 				vim.notify(
-					string.format("'%s %s' internal error: failed creating new buffer", args.name, args.fargs[1]),
+					string.format("'%s %s' internal error: failed creating new buffer", name, args[1]),
 					vim.log.levels.ERROR
 				)
 				print("Internal error: failed creating new buffer")
@@ -131,7 +142,7 @@ function M.setup(module_opts)
 					MiniNotify.remove(timer_note_id)
 				end
 				vim.notify(
-					string.format("'%s %s' executed, new buffer with id %d created", args.name, args.fargs[1], new_buf),
+					string.format("'%s %s' executed, new buffer with id %d created", name, args[1], new_buf),
 					vim.log.levels.INFO
 				)
 			end
@@ -164,7 +175,7 @@ function M.setup(module_opts)
 
 		if not (job > 0) then
 			vim.notify(
-				string.format("'%s %s' execution error...", args.name, args.fargs[1]),
+				string.format("'%s %s' execution error...", name, args[1]),
 				vim.log.levels.ERROR
 			)
 			print(string.format("Error executing: %s", sh_args))
@@ -180,11 +191,11 @@ function M.setup(module_opts)
 			timer:start(timer_delay_ms, timer_delay_ms, function ()
 				if timer_note_id then
 					MiniNotify.update(timer_note_id, {
-						msg = string.format("[%s] ':%s %s' running...", running, args.name, args.fargs[1])
+						msg = string.format("[%s] ':%s %s' running...", running, name, args[1])
 					})
 				else
 					vim.notify(
-						string.format("[%s] ':%s %s' running...", running, args.name, args.fargs[1]),
+						string.format("[%s] ':%s %s' running...", running, name, args[1]),
 						vim.log.levels.INFO
 					)
 				end
@@ -197,7 +208,7 @@ function M.setup(module_opts)
 			end)
 		else
 			while vim.fn.jobwait({ job }, print_delay_ms)[1] == -1 do
-				vim.api.nvim_echo({{string.format("[%s] Waiting for :%s %s", running, args.name, sh_args)}}, false, {})
+				vim.api.nvim_echo({{string.format("[%s] Waiting for :%s %s", running, name, sh_args)}}, false, {})
 
 				if running == "|" then running = "/"
 				elseif running == "/" then running = "-"
@@ -211,7 +222,7 @@ function M.setup(module_opts)
 	vim.api.nvim_create_user_command(
 		'Pipe',
 		function(args)
-			pipe_fn(args, { sync = true })
+			pipe_fn(args.name, args.fargs, { sync = true })
 		end,
 		{
 			nargs = '+',
@@ -221,11 +232,50 @@ function M.setup(module_opts)
 	vim.api.nvim_create_user_command(
 		'PipeA',
 		function(args)
-			pipe_fn(args, { sync = false })
+			pipe_fn(args.name, args.fargs, { sync = false })
 		end,
 		{
 			nargs = '+',
 			complete = 'shellcmdline',
+		})
+
+	local pipe_buf_cmd_prelude = function (args)
+		local buf_id = vim.api.nvim_get_current_buf()
+		if #args.fargs == 1 then
+			buf_id = tonumber(args.fargs[1], 10)
+			if not buf_id then
+				vim.notify(
+					string.format("'%s' error: '%s' not a valid buffer", args.name, args.fargs[1]),
+					vim.log.levels.ERROR
+				)
+				print(string.format("Error: %s not a valid buffer", args.fargs[1]))
+				return nil
+			end
+		end
+
+		return split_at_whitespaces(read_buffer(buf_id))
+	end
+
+	vim.api.nvim_create_user_command(
+		'PipeBuf',
+		function(args)
+			local cmd_args = pipe_buf_cmd_prelude(args)
+			if not cmd_args then return end
+			pipe_fn(args.name, cmd_args, { sync = true })
+		end,
+		{
+			nargs = '?',
+		})
+
+	vim.api.nvim_create_user_command(
+		'PipeBufA',
+		function(args)
+			local cmd_args = pipe_buf_cmd_prelude(args)
+			if not cmd_args then return end
+			pipe_fn(args.name, cmd_args, { sync = false })
+		end,
+		{
+			nargs = '?',
 		})
 
 end
